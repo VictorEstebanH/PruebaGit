4ac3ddfbb8413814ee3c4903df5e6412c388ea00
VictorEstebanH <vicesthol23@gmail.com>
2023-10-11 14:49:55 -0500

    commit1

---
+++ src/pruebagitcal/PruebaGitCal.java
@@ -0,0 +1,20 @@
+
+package pruebagitcal;
+
+
+public class PruebaGitCal {
+
+   
+    public static void main(String[] args) {
+    
+        int num1=0;
+        int num2=1;
+        int suma=num1+num2;
+        System.out.println(suma);
+        
+                
+        
+        
+    }
+    
+}
